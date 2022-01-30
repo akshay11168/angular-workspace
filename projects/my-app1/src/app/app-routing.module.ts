5327f1d744936cda87965b6a7570c6cd484c3be8
akshay biradar <akshay.b@signdesk.com>
2022-01-31 05:19:53 +0530

    added library to workspace

--- projects/my-app1/src/app/app-routing.module.ts
+++ projects/my-app1/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { View1Component } from './view1/view1.component';
 import { View2Component } from './view2/view2.component';
 
 const routes: Routes = [
-  {path:'', component:AppComponent},
+  {path:'', component:View1Component},
   {path:'view1', component:View1Component},
   {path:'view2', component:View2Component},
 ];
--- projects/my-app1/src/app/app.module.ts
+++ projects/my-app1/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MyLib1Module } from 'my-lib1';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { View2Component } from './view2/view2.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MyLib1Module
   ],
   providers: [],
   bootstrap: [AppComponent]
